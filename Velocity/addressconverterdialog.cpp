367c3cde980898eb406b9e488e7d824c57e530f9
Adam Spindler <xmeadamx@gmail.com>
2012-09-02 14:26:47 -0400

    added address converter dialog for XDBF

---
+++ Velocity/addressconverterdialog.cpp
@@ -0,0 +1,27 @@
+#include "addressconverterdialog.h"
+#include "ui_addressconverterdialog.h"
+
+AddressConverterDialog::AddressConverterDialog(XDBF *xdbf, QWidget *parent) : QDialog(parent), ui(new Ui::AddressConverterDialog), xdbf(xdbf)
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    ui->setupUi(this);
+
+    ui->spnRealAddr->setValue(xdbf->GetRealAddress(0));
+    ui->spnRealAddr->setMinimum(ui->spnRealAddr->value());
+
+    setFixedSize(sizeHint());
+}
+
+AddressConverterDialog::~AddressConverterDialog()
+{
+    delete ui;
+}
+void AddressConverterDialog::on_spnSpecifier_editingFinished()
+{
+    ui->spnRealAddr->setValue(xdbf->GetRealAddress(ui->spnSpecifier->value()));
+}
+
+void AddressConverterDialog::on_spnRealAddr_editingFinished()
+{
+    ui->spnSpecifier->setValue(xdbf->GetSpecifier(ui->spnRealAddr->value()));
+}
---
+++ Velocity/addressconverterdialog.h
@@ -0,0 +1,30 @@
+#ifndef ADDRESSCONVERTERDIALOG_H
+#define ADDRESSCONVERTERDIALOG_H
+
+#include <QDialog>
+
+#include "GPD/XDBF.h"
+
+namespace Ui {
+class AddressConverterDialog;
+}
+
+class AddressConverterDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit AddressConverterDialog(XDBF *xdbf, QWidget *parent = 0);
+    ~AddressConverterDialog();
+    
+private slots:
+    void on_spnSpecifier_editingFinished();
+
+    void on_spnRealAddr_editingFinished();
+
+private:
+    Ui::AddressConverterDialog *ui;
+    XDBF *xdbf;
+};
+
+#endif // ADDRESSCONVERTERDIALOG_H
