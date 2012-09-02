be844f0b54a247e8d00a63190a8f9a628d3e32c5
Stevie Hetelekides <hetelek@gmail.com>
2012-09-02 02:35:59 -0400

    dialog for viewing file/folder entry properties

---
+++ Velocity/propertiesdialog.cpp
@@ -0,0 +1,144 @@
+#include "propertiesdialog.h"
+#include "ui_propertiesdialog.h"
+
+PropertiesDialog::PropertiesDialog(FileEntry *entry, QString location, bool *changed, QIcon icon, bool hasChildren, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PropertiesDialog),
+    changed(changed),
+    entry(entry)
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    ui->setupUi(this);
+    setWindowTitle(QString::fromStdString(entry->name) + " Properties");
+    ui->lblIcon->setPixmap(icon.pixmap(24, 24));
+
+    // if it's a file, remove the filename
+    if (!(entry->flags & 2))
+    {
+        int index = location.lastIndexOf('\\');
+        if (index != -1)
+            location = location.mid(0, index);
+        else
+            location = "";
+    }
+    location.insert(0, '\\');
+    ui->leName->setText(QString::fromStdString(entry->name));
+
+    // get the extension
+    size_t index = entry->name.rfind('.');
+    QString exten;
+    if (index != string::npos)
+        exten = QString::fromStdString(entry->name.substr(index)).toLower();
+    else
+        exten = "File";
+
+    // get the friendly name of the extension (if any)
+    QString final;
+    if (exten != "File")
+    {
+        QString friendlyName;
+        if (exten == ".gpd")
+            friendlyName = "Game Progress Data";
+        else if (exten == ".png" || exten == ".jpg" || exten == ".jpeg")
+            friendlyName = "Image";
+        else if (exten == ".fit")
+            friendlyName = "Fitness File";
+        else
+            friendlyName = exten.mid(1).toUpper() + " File";
+        final = friendlyName + " (" + exten + ")";
+    }
+    else
+        final = exten;
+
+    ui->lblTypeOfFile->setText(final);
+    ui->lblLocation->setText(location);
+    ui->lblSize->setText(QString::fromStdString(ByteSizeToString(entry->fileSize)) + " (" + QString::number(entry->fileSize) + " bytes)");
+
+    int sizeOnDisk = (entry->fileSize + 0xFFF) & 0xFFFFF000;
+    ui->lblSizeOnDisk->setText(QString::fromStdString(ByteSizeToString(sizeOnDisk)) + " (" + QString::number(sizeOnDisk) + " bytes)");
+
+    // get the MSTime from
+    MSTime createdtime = DWORDToMSTime(entry->createdTimeStamp);
+    MSTime accesstime = DWORDToMSTime(entry->accessTimeStamp);
+    QDateTime dateTime;
+
+    QDate date;
+    QTime time;
+
+    date.setDate(createdtime.year, createdtime.month, createdtime.monthDay);
+    time.setHMS(createdtime.hours, createdtime.minutes, createdtime.seconds);
+
+    dateTime.setDate(date);
+    dateTime.setTime(time);
+
+    QString dateStr = dateTime.toString("dddd, MMMM dd, yyyy, h:mm:ss AP");
+    if (dateStr.isEmpty())
+        dateStr = "<i>N/A</i>";
+    ui->lblCreated->setText(dateStr);
+
+    date.setDate(accesstime.year, accesstime.month, accesstime.monthDay);
+    time.setHMS(accesstime.hours, accesstime.minutes, accesstime.seconds);
+
+    dateTime.setDate(date);
+    dateTime.setTime(time);
+
+    dateStr = dateTime.toString("dddd, MMMM dd, yyyy, h:mm:ss AP");
+    if (dateStr.isEmpty())
+        dateStr = "<i>N/A</i>";
+    ui->lblAccessed->setText(dateStr);
+
+    ui->cbConsecutive->setCheckState((entry->flags & 1) << 1);
+    ui->cbFolder->setCheckState((entry->flags & 2) << 1);
+
+    if (hasChildren)
+        ui->cbFolder->setEnabled(false);
+}
+
+PropertiesDialog::~PropertiesDialog()
+{
+    delete ui;
+}
+
+void PropertiesDialog::on_btnCancel_clicked()
+{
+    *changed = false;
+    close();
+}
+
+void PropertiesDialog::on_leName_textChanged(const QString &arg1)
+{
+    ui->btnApply->setEnabled(true);
+}
+
+void PropertiesDialog::on_cbConsecutive_toggled(bool checked)
+{
+    ui->btnApply->setEnabled(true);
+}
+
+void PropertiesDialog::on_cbFolder_toggled(bool checked)
+{
+    ui->btnApply->setEnabled(true);
+}
+
+void PropertiesDialog::updateEntry()
+{
+    entry->name = ui->leName->text().toStdString();
+
+    int consecState = ui->cbConsecutive->checkState();
+    int folderState = ui->cbFolder->checkState();
+
+    entry->flags = (consecState >> 1) | folderState;
+
+    *changed = true;
+}
+
+void PropertiesDialog::on_btnOK_clicked()
+{
+    updateEntry();
+    close();
+}
+
+void PropertiesDialog::on_btnApply_clicked()
+{
+    updateEntry();
+}
---
+++ Velocity/propertiesdialog.h
@@ -0,0 +1,41 @@
+#ifndef PROPERTIESDIALOG_H
+#define PROPERTIESDIALOG_H
+
+#include <QDialog>
+#include <QDate>
+#include <Stfs/StfsPackage.h>
+
+namespace Ui {
+class PropertiesDialog;
+}
+
+class PropertiesDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit PropertiesDialog(FileEntry *entry, QString location, bool *changed, QIcon icon, bool hasChildren, QWidget *parent = 0);
+    ~PropertiesDialog();
+    
+private slots:
+    void on_btnCancel_clicked();
+
+    void on_leName_textChanged(const QString &arg1);
+
+    void on_cbConsecutive_toggled(bool checked);
+
+    void on_cbFolder_toggled(bool checked);
+
+    void on_btnOK_clicked();
+
+    void on_btnApply_clicked();
+
+private:
+    void updateEntry();
+
+    Ui::PropertiesDialog *ui;
+    bool *changed;
+    FileEntry *entry;
+};
+
+#endif // PROPERTIESDIALOG_H
