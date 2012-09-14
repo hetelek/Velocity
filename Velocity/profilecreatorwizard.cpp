2a217ab44f948a224146856049f470220edca45f
Adam Spindler <xmeadamx@gmail.com>
2012-09-13 20:17:41 -0400

    updated UI

--- Velocity/profilecreatorwizard.cpp
+++ Velocity/profilecreatorwizard.cpp
@@ -7,9 +7,48 @@ ProfileCreatorWizard::ProfileCreatorWizard(QWidget *parent) :
 {
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     ui->setupUi(this);
+
+    connect(this, SIGNAL(currentIdChanged(int)), this, SLOT(onCurrentIdChanged(int)));
+    connect(this, SIGNAL(finished(int)), this, SLOT(onFinished(int)));
+
+    ui->lblSavePath->setText(QtHelpers::DesktopLocation() + "\\butthole.prf");
 }
 
 ProfileCreatorWizard::~ProfileCreatorWizard()
 {
     delete ui;
+}
+
+void ProfileCreatorWizard::onCurrentIdChanged(int id)
+{
+    switch (id)
+    {
+        case 2:
+            button(QWizard::NextButton)->setEnabled(ui->txtGamertag->text() != "");
+            break;
+    }
+}
+
+void ProfileCreatorWizard::onFinished(int status)
+{
+    if (status != 0)
+        return;
+
+    // create a new file
+   /* FileIO io(ui->lblSavePath->text().toStdString(), true);
+
+    // null out the first 0xD000 bytes
+    BYTE tempBuffer[0x1000] = {0};
+    for (DWORD i = 0; i < 0xD; i++)
+        io.write(tempBuffer, 0x1000);
+
+    // set up the metadata for the profile
+    StfsMetaData metadata(&io, false, false);
+    metadata.magic = CON;
+    metadata.certificate.ownerConsoleType = (ui->cmbxType->currentIndex() == 0) ? Retail : DevKit; */
+}
+
+void ProfileCreatorWizard::on_txtGamertag_textChanged(const QString &arg1)
+{
+    button(QWizard::NextButton)->setEnabled(ui->txtGamertag->text() != "");
 }
--- Velocity/profilecreatorwizard.h
+++ Velocity/profilecreatorwizard.h
@@ -1,7 +1,15 @@
 #ifndef PROFILECREATORWIZARD_H
 #define PROFILECREATORWIZARD_H
 
+// qt
 #include <QWizard>
+#include <QMessageBox>
+#include "qthelpers.h"
+
+// xbox360
+#include "Stfs/StfsPackage.h"
+#include "Stfs/StfsMetaData.h"
+#include "GPD/DashboardGPD.h"
 
 namespace Ui {
 class ProfileCreatorWizard;
@@ -15,6 +23,13 @@ public:
     explicit ProfileCreatorWizard(QWidget *parent = 0);
     ~ProfileCreatorWizard();
     
+private slots:
+    void onCurrentIdChanged(int id);
+
+    void on_txtGamertag_textChanged(const QString &arg1);
+
+    void onFinished(int status);
+
 private:
     Ui::ProfileCreatorWizard *ui;
 };
