5a16ab231c6cdd6174bc0c4df4bd26a5d737788a
Adam Spindler <xmeadamx@gmail.com>
2012-09-20 18:57:59 -0400

    created class for downloading assets

---
+++ Velocity/AvatarAssetDownloader.h
@@ -0,0 +1,42 @@
+#ifndef AVATARASSETDOWNLOADER_H
+#define AVATARASSETDOWNLOADER_H
+
+// qt
+#include <QObject>
+#include <QHttp>
+#include <QUuid>
+#include <QUrl>
+#include <QDir>
+
+// other
+#include "winnames.h"
+#include "FileIO.h"
+
+class AvatarAssetDownloader : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AvatarAssetDownloader(QString titleID, QString guid, QObject *parent = 0);
+    QString GetV1TempPath();
+    QString GetV2TempPath();
+
+signals:
+    void FinishedDownloading();
+
+public slots:
+    void onDone(bool);
+    void onRequestFinished(int, bool);
+private:
+    QHttp *http;
+
+    QString titleID;
+    QString guid;
+
+    QString v1TempPath;
+    QString v2TempPath;
+
+    bool v1Done;
+    bool v2Done;
+};
+
+#endif // AVATARASSETDOWNLOADER_H
