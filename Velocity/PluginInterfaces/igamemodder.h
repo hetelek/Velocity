f65d720c51482de58f71c152496e74da43d64134
Stevie Hetelekides <hetelek@gmail.com>
2012-10-07 19:13:19 -0400

    updated completely

--- Velocity/PluginInterfaces/igamemodder.h
+++ Velocity/PluginInterfaces/igamemodder.h
@@ -1,22 +1,24 @@
 #ifndef IGAMEMODDER_H
 #define IGAMEMODDER_H
 
+#include <QString>
 #include <QWidget>
-#include <Stfs/StfsPackage.h>
+
+#include "Stfs/StfsPackage.h"
 
 class IGameModder
 {
 public:
-    virtual IGameModder(StfsPackage *package, QWidget *parent = NULL) { }
+    virtual IGameModder() { }
     virtual ~IGameModder() { }
 
+    virtual void LoadPackage(StfsPackage *package, QWidget *parent = NULL) const = 0;
+    virtual QWidget* GetDialog() const = 0;
     virtual QString ToolAuthor() const = 0;
     virtual QString ToolName() const = 0;
     virtual DWORD TitleID() const = 0;
-
-    virtual bool Save() = 0;
 };
 
-Q_DECLARE_INTERFACE (IGameModder, "com.velocity.GameModders")
+Q_DECLARE_INTERFACE (IGameModder, "com.exetelek.Veloctiy.IGameModder/0.1")
 
-#endif // IGAMEMODDER_H
+#endif
