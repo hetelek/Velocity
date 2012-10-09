f015c8480db63d746a310c9cb3cd22d0a8f920d7
Stevie Hetelekides <hetelek@gmail.com>
2012-10-09 01:21:50 -0400

    setting the working directory didn't work. :`(

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -1,11 +1,8 @@
 #include <QtGui/QApplication>
-#include <QDir>
-#include <QCoreApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QDir::setCurrent(QCoreApplication::applicationFilePath());
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
