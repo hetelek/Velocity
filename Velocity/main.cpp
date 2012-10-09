0405e5cff97db4fcad968b9636b390f02b9a88b6
Stevie Hetelekides <hetelek@gmail.com>
2012-10-09 01:06:53 -0400

    working directory is now set as the application executable directory

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -1,8 +1,11 @@
 #include <QtGui/QApplication>
+#include <QDir>
+#include <QCoreApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QDir::setCurrent(QCoreApplication::applicationFilePath());
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
