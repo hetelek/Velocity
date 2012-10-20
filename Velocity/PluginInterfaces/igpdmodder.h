f1b100e53ea8ba8ffd348ebb79206351b95206c1
Stevie Hetelekides <hetelek@gmail.com>
2012-10-19 23:32:57 -0400

    didn't make all LoadGPD and GetDialog constant

--- Velocity/PluginInterfaces/igpdmodder.h
+++ Velocity/PluginInterfaces/igpdmodder.h
@@ -5,12 +5,7 @@
 #include <QWidget>
 
 // xbox360
-#include "GPD/XDBF.h"
-#include "GPD/XDBFDefininitions.h"
-#include "GPD/XDBFHelpers.h"
-#include "GPD/GPDBase.h"
 #include "GPD/GameGPD.h"
-#include "FileIO.h"
 
 class IGPDModder
 {
@@ -18,8 +13,8 @@ public:
     IGPDModder() { }
     virtual ~IGPDModder() { }
 
-    virtual void LoadGPD(GameGPD *gpd, QWidget *parent = NULL) const = 0;
-    virtual QWidget* GetDialog() const = 0;
+    virtual void LoadGPD(GameGPD *gpd) { }
+    virtual QWidget* GetDialog() { }
     virtual QString ToolAuthor() const = 0;
     virtual QString ToolName() const = 0;
     virtual DWORD TitleID() const = 0;
