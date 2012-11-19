1628ce7269f647f64a0a4a35c0a26eacd2dd8013
Stevie Hetelekides <hetelek@gmail.com>
2012-11-18 21:11:27 -0500

    Added 'Version'

--- Velocity/PluginInterfaces/igpdmodder.h
+++ Velocity/PluginInterfaces/igpdmodder.h
@@ -16,10 +16,12 @@ public:
     virtual ~IGPDModder() { }
 
     virtual void LoadGPD(GameGPD * /* gpd*/, bool */*ok */= NULL, void * /*args*/ = NULL) { }
-    virtual QDialog* GetDialog() { return NULL; }
+    virtual QWidget* GetDialog() { return NULL; }
+    virtual QString Version() const = 0;
     virtual QString ToolAuthor() const = 0;
     virtual QString ToolName() const = 0;
     virtual DWORD TitleID() const = 0;
+
 };
 
 Q_DECLARE_INTERFACE (IGPDModder, "com.exetelek.Veloctiy.IGPDModder/0.1")
