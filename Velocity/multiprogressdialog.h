21bb47e8dd769a87e9f33575b9b1c303f1ad8735
Adam Spindler <xmeadamx@gmail.com>
2013-01-04 22:04:18 -0500

    Added progress dialog for multiple files

---
+++ Velocity/multiprogressdialog.h
@@ -0,0 +1,44 @@
+#ifndef MULTIPROGRESSDIALOG_H
+#define MULTIPROGRESSDIALOG_H
+
+// qt
+#include <QDialog>
+#include <QMessageBox>
+
+// xbox
+#include "Stfs/XContentHeader.h"
+#include "Disc/svod.h"
+#include "IO/SvodIO.h"
+
+namespace Ui {
+class MultiProgressDialog;
+}
+
+void updateProgress(void *form, DWORD curProgress, DWORD total);
+
+class MultiProgressDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit MultiProgressDialog(FileSystem fileSystem, void *device, QString outDir, QList<void*> filesToExtract, QWidget *parent = 0);
+    ~MultiProgressDialog();
+
+    void start();
+    
+private:
+    Ui::MultiProgressDialog *ui;
+    FileSystem system;
+    void *device;
+    QString outDir;
+    QList<void*> filesToExtract;
+    int fileIndex;
+    DWORD overallProgress;
+    DWORD overallProgressTotal;
+
+    void extractNextFile();
+
+    friend void updateProgress(void *form, DWORD curProgress, DWORD total);
+};
+
+#endif // MULTIPROGRESSDIALOG_H
