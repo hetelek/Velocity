26487af5324db8bd2219d56438e380d53c7c8f9b
Adam Spindler <xmeadamx@gmail.com>
2013-01-04 22:05:26 -0500

    Renamed to StfsVolumeDescriptorDialog

---
+++ Velocity/stfsvolumedescriptordialog.h
@@ -0,0 +1,42 @@
+#ifndef VOLUMEDESCRIPTORDIALOG_H
+#define VOLUMEDESCRIPTORDIALOG_H
+
+// qt
+#include <QDialog>
+#include <QComboBox>
+#include <QMessageBox>
+#include <QStatusBar>
+#include "qthelpers.h"
+
+// xbox360
+#include "Stfs/StfsDefinitions.h"
+
+namespace Ui {
+class VolumeDescriptorDialog;
+}
+
+class StfsVolumeDescriptorDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit StfsVolumeDescriptorDialog(QStatusBar *statusBar, StfsVolumeDescriptor *desc, QWidget *parent = 0);
+    ~StfsVolumeDescriptorDialog();
+    
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+    void packageTypeChanged(int index);
+
+private:
+    Ui::VolumeDescriptorDialog *ui;
+    StfsVolumeDescriptor *desc;
+    QStatusBar *statusBar;
+
+    QComboBox *cmbxHashTable;
+    QComboBox *cmbxPackageType;
+};
+
+#endif // VOLUMEDESCRIPTORDIALOG_H
