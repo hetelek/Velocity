caf8dd77ca4342f3eb314378f17d9f6bd1416fe9
Stevie Hetelekides <hetelek@gmail.com>
2013-02-15 20:01:59 -0500

    Example of file extracting

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -8,6 +8,32 @@
 int main(int argc, char *argv[])
 {
     FatxDrive drive(L"\\\\.\\PHYSICALDRIVE1");
+    Partition *part = drive.GetPartitions().at(0);
+
+    drive.GetChildFileEntries(&part->root);
+    FatxFileEntry entry = part->root.cachedFiles.at(0);
+
+    drive.GetChildFileEntries(&entry);
+    FatxFileEntry folder = entry.cachedFiles.at(0);
+
+    drive.GetChildFileEntries(&folder);
+
+    FatxIO io = drive.GetFatxIO(&folder.cachedFiles.at(0));
+
+    io.SetPosition(0);
+    FileIO newFile("C:/Users/Stevie/Desktop/extracted.dat", true);
+
+    BYTE buffer[0x10000];
+    DWORD size = io.GetFatxFileEntry().fileSize;
+    while (size > 0)
+    {
+        DWORD amount = (size < 0x10000) ? size : 0x10000;
+        io.ReadBytes(buffer, amount);
+        newFile.write(buffer, amount);
+        size -= amount;
+    }
+
+    newFile.close();
 
     QApplication a(argc, argv);
     a.addLibraryPath(":/plugins/imageformats");
