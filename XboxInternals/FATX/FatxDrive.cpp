1de5c3c4026285fabcbf8310af96d6aed4e80f5c
Adam Spindler <xmeadamx@gmail.com>
2013-02-22 23:23:01 -0500

    Added credit at the top of the files

--- XboxInternals/FATX/FatxDrive.cpp
+++ XboxInternals/FATX/FatxDrive.cpp
@@ -1,3 +1,6 @@
+/* Most parts of this class were originally developed by Lander Griffith (https://github.com/landr0id/).
+   Much of his code is used throughout this class or very slightly modified */
+
 #include "FatxDrive.h"
 
 FatxDrive::FatxDrive(std::string drivePath)
--- XboxInternals/FATX/FatxDrive.h
+++ XboxInternals/FATX/FatxDrive.h
@@ -1,3 +1,6 @@
+/* Most parts of this class were originally developed by Lander Griffith (https://github.com/landr0id/).
+   Much of his code is used throughout this class or very slightly modified */
+
 #ifndef FATXDRIVE_H
 #define FATXDRIVE_H
 
