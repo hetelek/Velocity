83fb776928d0536a8c0c8cb4d3420b42bfd2a429
Stevie Hetelekides <hetelek@gmail.com>
2013-03-11 23:49:37 -0400

    Started drive detection

--- XboxInternals/FATX/fatxhelpers.cpp
+++ XboxInternals/FATX/fatxhelpers.cpp
@@ -1,4 +1,4 @@
 /* Most parts of this class were originally developed by Lander Griffith (https://github.com/landr0id/).
    Much of his code is used throughout this class or very slightly modified */
 
-#include "fatxhelpers.h"
+#include "FatxHelpers.h"
