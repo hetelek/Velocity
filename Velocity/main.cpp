875278682df181b811bba3999c8b120824a53a10
Adam Spindler <xmeadamx@gmail.com>
2013-06-17 12:59:00 -0400

    Added debug code

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -3,8 +3,35 @@
 #include <botan/botan.h>
 #include "mainwindow.h"
 
+#include <QDebug>
+#include "Fatx/XContentDevice.h"
+#include "Fatx/FatxDriveDetection.h"
+
 int main(int argc, char *argv[])
 {
+    try
+    {
+        XContentDevice device(FatxDriveDetection::GetAllFatxDrives().at(0));
+        device.LoadDevice();
+
+        for (int i = 0; i < device.profiles.size(); i++)
+        {
+            qDebug() << QString::fromStdWString(device.profiles.at(i).GetName());
+            for (int x = 0; x < device.profiles.at(i).titles.size(); x++)
+            {
+                XContentDeviceTitle title = device.profiles.at(i).titles.at(x);
+                qDebug() << "\t" << QString::fromStdWString(title.GetName()) << " - " << QString::number(title.GetTitleID(), 16);
+                for (int y = 0; y < title.titleSaves.size(); y++)
+                    qDebug() << "\t\t" << QString::fromStdWString(title.titleSaves.at(y).GetName());
+            }
+        }
+    }
+    catch (std::string error)
+    {
+        qDebug() << QString::fromStdString(error);
+    }
+
+
     QApplication a(argc, argv);
     a.addLibraryPath(":/plugins/imageformats");
 
