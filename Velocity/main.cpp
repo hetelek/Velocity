de19852ee5fc14b0ac688e195c06cdeced620206
Adam Spindler <xmeadamx@gmail.com>
2013-06-17 15:14:52 -0400

    Modified debug code to refelect changes

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
         XContentDevice device(FatxDriveDetection::GetAllFatxDrives().at(0));
         device.LoadDevice();
 
-        for (int i = 0; i < device.profiles.size(); i++)
+        for (int i = 0; i < device.profiles->size(); i++)
         {
-            qDebug() << QString::fromStdWString(device.profiles.at(i).GetName());
-            for (int x = 0; x < device.profiles.at(i).titles.size(); x++)
+            qDebug() << QString::fromStdWString(device.profiles->at(i).GetName());
+            for (int x = 0; x < device.profiles->at(i).titles.size(); x++)
             {
-                XContentDeviceTitle title = device.profiles.at(i).titles.at(x);
+                XContentDeviceTitle title = device.profiles->at(i).titles.at(x);
                 qDebug() << "\t" << QString::fromStdWString(title.GetName()) << " - " << QString::number(title.GetTitleID(), 16);
                 for (int y = 0; y < title.titleSaves.size(); y++)
                     qDebug() << "\t\t" << QString::fromStdWString(title.titleSaves.at(y).GetName());
