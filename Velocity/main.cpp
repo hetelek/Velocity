05b201ac0a572e5b27e0e10fde92a8df70ac702e
Adam Spindler <xmeadamx@gmail.com>
2013-06-18 14:00:22 -0400

    Removed debug code

--- Velocity/main.cpp
+++ Velocity/main.cpp
@@ -7,48 +7,8 @@
 #include "Fatx/XContentDevice.h"
 #include "Fatx/FatxDriveDetection.h"
 
-void printCategory(QString category, std::vector<XContentDeviceSharedItem> *items)
-{
-    qDebug() << "\t" << category;
-    for (int i = 0; i < items->size(); i++)
-        qDebug() << "\t\t" << QString::fromStdWString(items->at(i).GetName());
-}
-
 int main(int argc, char *argv[])
 {
-    try
-    {
-        XContentDevice device(FatxDriveDetection::GetAllFatxDrives().at(0));
-        device.LoadDevice();
-
-        for (int i = 0; i < device.profiles->size(); i++)
-        {
-            qDebug() << QString::fromStdWString(device.profiles->at(i).GetName());
-            for (int x = 0; x < device.profiles->at(i).titles.size(); x++)
-            {
-                XContentDeviceTitle title = device.profiles->at(i).titles.at(x);
-                qDebug() << "\t" << QString::fromStdWString(title.GetName()) << " - " << QString::number(title.GetTitleID(), 16);
-                for (int y = 0; y < title.titleSaves.size(); y++)
-                    qDebug() << "\t\t" << QString::fromStdWString(title.titleSaves.at(y).GetName());
-            }
-        }
-
-        qDebug() << "Shared Items";
-        printCategory("Games", device.games);
-        printCategory("DLC", device.dlc);
-        printCategory("Demos", device.demos);
-        printCategory("Videos", device.videos);
-        printCategory("Themes", device.themes);
-        printCategory("Gamer Pictures", device.gamerPictures);
-        printCategory("Avatar Items", device.avatarItems);
-        printCategory("System Items", device.systemItems);
-    }
-    catch (std::string error)
-    {
-        qDebug() << QString::fromStdString(error);
-    }
-
-
     QApplication a(argc, argv);
     a.addLibraryPath(":/plugins/imageformats");
 
