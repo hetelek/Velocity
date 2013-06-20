7078a4e5fd6e6eb65a65c1aaf686ab920a0dad19
Adam Spindler <xmeadamx@gmail.com>
2013-06-19 21:12:24 -0400

    Title icons are now correct

--- XboxInternals/Fatx/XContentDeviceTitle.cpp
+++ XboxInternals/Fatx/XContentDeviceTitle.cpp
@@ -17,14 +17,14 @@ BYTE *XContentDeviceTitle::GetThumbnail()
 {
     if (titleSaves.size() == 0)
         return NULL;
-    return titleSaves.at(0).package->metaData->thumbnailImage;
+    return titleSaves.at(0).package->metaData->titleThumbnailImage;
 }
 
 DWORD XContentDeviceTitle::GetThumbnailSize()
 {
     if (titleSaves.size() == 0)
         return 0;
-    return titleSaves.at(0).package->metaData->thumbnailImageSize;
+    return titleSaves.at(0).package->metaData->titleThumbnailImageSize;
 }
 
 DWORD XContentDeviceTitle::GetTitleID()
