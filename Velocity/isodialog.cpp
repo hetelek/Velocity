23f16036c56e9be1ad865f67c1c6042fc5f9390c
Gualdimar <multimeter42@gmail.com>
2016-10-27 00:31:13 +0300

    Revert "Fix bug with extracting single file on OS X"
    This reverts commit 5c372beafc77a2f112a4f3be50e707c92661e711.

--- Velocity/isodialog.cpp
+++ Velocity/isodialog.cpp
@@ -122,10 +122,6 @@ void ISODialog::showContextMenu(QPoint point)
 
         QString outDirectory = QFileDialog::getExistingDirectory(this, "Choose a place to extract the file to", QtHelpers::DesktopLocation());
 
-#ifndef _WIN32
-        outDirectory += "/";
-#endif
-
         SingleProgressDialog *progressDialog = new SingleProgressDialog(FileSystemISO, iso, OpExtract, pathInISO, outDirectory, NULL, this);
         progressDialog->setModal(true);
         progressDialog->show();
