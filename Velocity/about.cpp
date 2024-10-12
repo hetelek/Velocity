b15e02e566fde18488a88b3c3ee214bc614cad26
Pandoriaantje <pandoriaantje@gmail.com>
2024-10-12 14:33:52 +0200

    Phonon is deprecated
    removed Phonon dependency.

--- Velocity/about.cpp
+++ Velocity/about.cpp
@@ -9,22 +9,6 @@ About::About(QWidget *parent) :
     setFixedSize(sizeHint());
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     QtHelpers::GenAdjustWidgetAppearanceToOS(this);
-
-#ifdef _WIN32
-    if (!QFile::exists(QtHelpers::ExecutingDirectory() + "Developers.mp3"))
-        return;
-
-    developers = new Phonon::MediaObject(this);
-    audioOutput = new Phonon::AudioOutput(Phonon::MusicCategory, this);
-
-    connect(developers, SIGNAL(finished()), this, SLOT(onSongFinished()));
-
-    Phonon::createPath(developers, audioOutput);
-
-    developers->setCurrentSource(Phonon::MediaSource(QtHelpers::ExecutingDirectory() +
-            "Developers.mp3"));
-    developers->play();
-#endif
 }
 
 About::~About()
@@ -35,13 +19,6 @@ About::~About()
 
 void About::onSongFinished()
 {
-#ifdef _WIN32
-    // for (;;) let's listen to it one more time;
-    developers->clear();
-    developers->setCurrentSource(Phonon::MediaSource(QtHelpers::ExecutingDirectory() +
-            "Developers.mp3"));
-    developers->play();
-#endif
 }
 
 void About::on_label_3_linkActivated(const QString &link)
