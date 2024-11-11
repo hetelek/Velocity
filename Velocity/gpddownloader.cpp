9e0a12a16ae4da6e95778588d86106941e96fdae
Pandoriaantje <pandoriaantje@gmail.com>
2024-11-11 15:53:37 +0100

    Added modernization for gameadder
    TODO: get the adder to actually work... for the life of me, I can't get it to function properly. But at least the code is half functioning, instead of not at all. This can (and will break your profile, until i can get it to properly finalize the injection.)

--- Velocity/gpddownloader.cpp
+++ Velocity/gpddownloader.cpp
@@ -1,72 +1,64 @@
 #include "gpddownloader.h"
 
-const QString GpdDownloader::BASE_GITHUB_URL = "https://raw.githubusercontent.com/Pandoriaantje/xbox360-gpd-files/main";
-
+// Constructor initializes the GpdDownloader object, setting up the network manager for handling requests
 GpdDownloader::GpdDownloader(TitleEntry entry, int index, bool hasAwards, QObject *parent)
-    : QObject(parent), entry(entry), hasAwards(hasAwards), gpdWritten(false), indexIn(index) {
-    gpdDirectory = "/gameadder/";
-    networkManager = new QNetworkAccessManager(this);
-
-    // Connect the finished signal to the slot for handling the response
-    connect(networkManager, &QNetworkAccessManager::finished, this, &GpdDownloader::onRequestFinished);
+    : QObject(parent), entry(std::move(entry)), indexIn(index), hasAwards(hasAwards) {
+    // Connect the finished signal from the network manager to the onRequestFinished slot
+    connect(&networkManager, &QNetworkAccessManager::finished, this, &GpdDownloader::onRequestFinished);
 }
 
+// Starts the download process for the game's GPD file
 void GpdDownloader::BeginDownload() {
-    QString url = QString("%1/game/%2.gpd")
-    .arg(BASE_GITHUB_URL, QString::number(entry.titleID, 16).toUpper());
-    QNetworkRequest request((QUrl(url)));
-    networkManager->get(request);
+    // Construct the URL to download the game's GPD file from the GitHub repository
+    QString url = QString("%1/game/%2.gpd").arg(BASE_GITHUB_URL, QString::number(entry.titleID, 16).toUpper());
+    QNetworkRequest request((QUrl(url)));  // Create a network request using the URL
+    networkManager.get(request);           // Send the GET request
 }
 
-int GpdDownloader::index() {
-    return indexIn;
-}
-
+// Slot called when the network request is finished
 void GpdDownloader::onRequestFinished(QNetworkReply *reply) {
+    // Check for errors in the network reply
     if (reply->error() != QNetworkReply::NoError) {
-        // Handle network error
+        // If there's an error, emit the FinishedDownloading signal with an error flag
         emit FinishedDownloading("", "", entry, true);
-        reply->deleteLater();
+        reply->deleteLater();  // Clean up the network reply object
         return;
     }
 
-    QString tempPath = QDir::tempPath() + "/" + QUuid::createUuid().toString().remove("{").remove("}").remove("-");
+    // Generate a temporary path to save the downloaded GPD file
+    QString tempPath = QDir::tempPath() + "/" + QUuid::createUuid().toString(QUuid::WithoutBraces);
 
+    // Check if the game GPD file has already been written
     if (!gpdWritten) {
+        // Save the game GPD file to the temporary path
         gameGpd = tempPath;
-
         QFile file(tempPath);
-        if (file.open(QFile::Truncate | QFile::WriteOnly)) {
-            file.write(reply->readAll());
-            file.close();
+        if (file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+            file.write(reply->readAll());  // Write the content of the reply to the file
         }
+        file.close();
+        gpdWritten = true;  // Mark the GPD file as written
 
-        gpdWritten = true;
+        // If the game has avatar awards, initiate a second download for the award GPD file
         if (hasAwards) {
-            // Download the avatar award GPD
-            QString awardUrl = QString("%1/award/%2.gpd")
-                                   .arg(BASE_GITHUB_URL, QString::number(entry.titleID, 16).toUpper());
+            QString awardUrl = QString("%1/award/%2.gpd").arg(BASE_GITHUB_URL, QString::number(entry.titleID, 16).toUpper());
             QNetworkRequest awardRequest((QUrl(awardUrl)));
-            networkManager->get(awardRequest);
+            networkManager.get(awardRequest);  // Send the GET request for the award GPD file
         } else {
+            // If there are no awards, emit the FinishedDownloading signal
             emit FinishedDownloading(gameGpd, "", entry, false);
         }
     } else {
+        // Save the award GPD file to the temporary path
         awardGpd = tempPath;
-
         QFile file(tempPath);
-        if (file.open(QFile::Truncate | QFile::WriteOnly)) {
-            file.write(reply->readAll());
-            file.close();
+        if (file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+            file.write(reply->readAll());  // Write the content of the reply to the file
         }
-
+        file.close();
+        // Emit the FinishedDownloading signal with both the game and award GPD paths
         emit FinishedDownloading(gameGpd, awardGpd, entry, false);
     }
 
-    reply->deleteLater();
-}
-
-void GpdDownloader::onDone() {
-    // Emit the signal indicating that the download is complete
-    emit FinishedDownloading(gameGpd, awardGpd, entry, false);
+    reply->deleteLater();  // Clean up the network reply object
 }
--- Velocity/gpddownloader.h
+++ Velocity/gpddownloader.h
@@ -1,44 +1,49 @@
-#ifndef GpdDOWNLOADER_H
-#define GpdDOWNLOADER_H
+#ifndef GPDDOWNLOADER_H
+#define GPDDOWNLOADER_H
 
-// qt
+#include "Gpd/XdbfDefininitions.h"
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 #include <QFile>
 #include <QDir>
 #include <QUuid>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-
-// other
-#include "winnames.h"
-#include "Gpd/DashboardGpd.h"
-#include "IO/FileIO.h"
+#include <QString>
 
 class GpdDownloader : public QObject {
     Q_OBJECT
+
 public:
+    // Constructor
     explicit GpdDownloader(TitleEntry entry, int index, bool hasAwards, QObject *parent = nullptr);
+
+    // Start the download process
     void BeginDownload();
-    int index();
+
+    // Get the index of the download
+    [[nodiscard]] int index() const noexcept { return indexIn; }
 
 signals:
-    void FinishedDownloading(const QString &gameGpd, const QString &awardGpd, TitleEntry entry, bool error);
+    void FinishedDownloading(QString gamePath, QString awardPath, TitleEntry entry, bool error);
 
 private slots:
+    // Handle the network request completion
     void onRequestFinished(QNetworkReply *reply);
-    void onDone();
 
 private:
-    static const QString BASE_GITHUB_URL;
+    // Static base URL for GPD files
+    static inline const QString BASE_GITHUB_URL = "https://raw.githubusercontent.com/Pandoriaantje/xbox360-gpd-files/main";
+
     TitleEntry entry;
-    bool hasAwards;
-    bool gpdWritten;
     int indexIn;
+    bool hasAwards;
+    bool gpdWritten = false;
+
     QString gameGpd;
     QString awardGpd;
-    QString gpdDirectory;
-    QNetworkAccessManager *networkManager;
+    QString gpdDirectory = "/gameadder/";
+
+    QNetworkAccessManager networkManager;
 };
 
 #endif // GPDDOWNLOADER_H
